160ae4353b71299411cc9ba3d08910b1fd7a07ed
ManYaPOV0 <149012099+ManYaPOV0@users.noreply.github.com>
2024-02-13 14:55:29 +0100

    Histogramme

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/rapport2/")
+@app.route("/histogramme/")
 def mongraphique2():
     return render_template("graphique2.html")
 
